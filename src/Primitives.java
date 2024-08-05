2ec9c7b204eb20972ac38531e8c93ae849fc2152
jasper <whiteana001@warren.k12.in.us>
2024-08-05 12:57:31 -0400

    code

--- src/Primitives.java
+++ src/Primitives.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Primitives {
+public class Primitives {
+    public static void main(String[] args) {
+
+        boolean myBoolean = true;
+        boolean myBoolean2 = false;
+
+        int myInt = 7;
+        System.out.println(myInt);
+        short myShort = 8;
+        System.out.println(myShort);
+        byte myByte = 9;
+        System.out.println(myByte);
+        long myLong = 6767676767676767676L;
+
+        char myChar = 'a';
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE + 1);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        int myOtherInt3 = 1_234_578;
+    }
 }
